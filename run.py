8b4a2a0d4dd9e1c285f4425023695ee57e2f9a5e
Schmalodev <132712267+Schmalodev@users.noreply.github.com>
2025-02-08 16:34:50 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import pyautogui
+import time
+
+app = Flask(__name__)
+
+@app.route("/deploy")
+def deploy():
+    pyautogui.keyDown('win')
+    pyautogui.keyDown('r')
+    time.sleep(5)
+    pyautogui.keyUp("win")
+    pyautogui.keyUp("r")
+    time.sleep(5)
+    pyautogui.press("enter")
+    time.sleep(5)
+    pyautogui.write("cd Desktop")
+    time.sleep(5)
+    pyautogui.press("enter")
+    time.sleep(5)
+    pyautogui.write("python donwload.py")
+    time.sleep(5)
+    pyautogui.press("enter")
+    pyautogui.press("alt")
+    pyautogui.press("F4")
+
+if __name__ == "__main__":
+    app.run(debug=True)
