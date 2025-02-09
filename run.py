6d47489e92f7b61b745ef6ccebdf64f6734f1d0a
Marlon Dinter <m.dinter@simplicity.ag>
2025-02-09 16:12:51 +0100

    add new Service

--- run.py
+++ run.py
@@ -4,9 +4,12 @@ import time
 
 app = Flask(__name__)
 
-@app.route("/building")
+@app.route("/sort", methods=["POST", "GET"])
 def func():
-    return "is Building"
+    return render_template("sort_item.html")
+
+
+
 
 # DEPLOY (DONT TOUCH)
 @app.route("/deploy", methods=["POST", "GET"])
@@ -45,4 +48,5 @@ def exit():
     return ""
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    """app.run(host="0.0.0.0", port=5000)"""
+    app.run(debug=True)
