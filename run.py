7a44db114f3dc5705a2c435857d685960439b3ba
Schmalodev <132712267+Schmalodev@users.noreply.github.com>
2025-02-09 15:50:46 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -28,5 +28,20 @@ def deploy():
     pyautogui.press("enter")
     return render_template("index.html")
 
+@app.route("/exit")
+def exit():
+    time.sleep(5)
+    pyautogui.keyDown("alt")
+    pyautogui.press("tab")
+    time.sleep(5)
+    pyautogui.keyUp("alt")
+    time.sleep(5)
+
+    pyautogui.keyDown("alt")
+    pyautogui.press("F4")
+    time.sleep(5)
+    pyautogui.keyUp("alt")
+    return ""
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
